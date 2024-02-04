2907492986ae757fe73936ca0ec760cafb64ef79
Ew4n1011 <ewanlee2005@gmail.com>
2024-02-04 19:25:00 +0100

    adding sddm theme option

--- Scripts/install_pst.sh
+++ Scripts/install_pst.sh
@@ -15,13 +15,21 @@ fi
 if pkg_installed sddm
     then
 
+    echo -e "\033[0;32m[DISPLAYMANAGER]\033[0m detected // sddm"
     if [ ! -d /etc/sddm.conf.d ] ; then
         sudo mkdir -p /etc/sddm.conf.d
     fi
 
     if [ ! -f /etc/sddm.conf.d/kde_settings.t2.bkp ] ; then
-        echo "configuring sddm..."
-        sudo tar -xzf ${CloneDir}/Source/arcs/Sddm_Ittu.tar.gz -C /usr/share/sddm/themes/
+        echo -e "\033[0;32m[DISPLAYMANAGER]\033[0m configuring sddm..."
+        echo -e "Select sddm theme:\n1) Ittu\n2) Candy\n3) Corners"
+        read -p "Enter option number : " sddmopt
+
+        case $sddmopt in
+        1) sddmtheme="Ittu";;
+        2) sddmtheme="Candy";;
+        *) sddmtheme="Corners";;
+        esac
 
         if [ -f /etc/sddm.conf.d/kde_settings.conf ] ; then
             sudo cp /etc/sddm.conf.d/kde_settings.conf /etc/sddm.conf.d/kde_settings.t2.bkp
@@ -29,12 +37,7 @@ if pkg_installed sddm
         sudo cp /usr/share/sddm/themes/ittu/kde_settings.conf /etc/sddm.conf.d/
         setfacl -m u:sddm:x /home/${USER}
     else
-        echo "sddm is already configured..."
-    fi
-
-    if [ ! -f /usr/share/sddm/themes/ittu/components/artwork/gifs/${USER}.gif ] ; then
-        sudo cp $(find /usr/share/sddm/themes/ittu/components/artwork/gifs -name "*.gif" | sort -R | head -1) /usr/share/sddm/themes/ittu/components/artwork/gifs/${USER}.gif
-        echo "avatar set for ${USER}..."
+        echo -e "\033[0;33m[SKIP]\033[0m sddm is already configured..."
     fi
 
     # Set keyboard layout to es
@@ -42,7 +45,7 @@ if pkg_installed sddm
         sudo localectl set-x11-keymap es
     fi
 else
-    echo "WARNING: sddm is not installed..."
+    echo -e "\033[0;33m[WARNING]\033[0m sddm is not installed..."
 fi
 
 
@@ -50,11 +53,12 @@ fi
 if pkg_installed dolphin && pkg_installed xdg-utils
     then
 
+    echo -e "\033[0;32m[FILEMANAGER]\033[0m detected // dolphin"
     xdg-mime default org.kde.dolphin.desktop inode/directory
-    echo "setting" `xdg-mime query default "inode/directory"` "as default file explorer..."
+    echo -e "\033[0;32m[FILEMANAGER]\033[0m setting" `xdg-mime query default "inode/directory"` "as default file explorer..."
 
 else
-    echo "WARNING: dolphin is not installed..."
+    echo -e "\033[0;33m[WARNING]\033[0m dolphin is not installed..."
 fi
 
 # swappy
@@ -62,10 +66,10 @@ if pkg_installed swappy
     then
     xdg-mime default swappy.desktop image/png
     xdg-mime default swappy.desktop image/jpeg
-    echo "setting" `xdg-mime query default "image/png"` "as default image viewer (png)..."
-    echo "setting" `xdg-mime query default "image/jpeg"` "as default image viewer (jpeg)..."
+    echo -e "\033[0;32m[FILEMANAGER]\033[0m setting" `xdg-mime query default "image/png"` "as default image viewer (png)..."
+    echo -e "\033[0;32m[FILEMANAGER]\033[0m setting" `xdg-mime query default "image/jpeg"` "as default image viewer (jpeg)..."
 else
-    echo "WARNING: swappy is not installed..."
+    echo -e "\033[0;33m[WARNING]\033[0m swappy is not installed..."
 fi
 
 # gparted
@@ -74,7 +78,7 @@ if pkg_installed gparted
     sudo sed -i "Exec=/usr/bin/gparted %f/c\Exec=sudo -E /usr/bin/gparted" /usr/share/applications/gparted.desktop
     sudo sh -c "echo '$(whoami) ALL=NOPASSWD:SETENV: /usr/bin/gparted' >> /etc/sudoers"
 else
-    echo "WARNING: gparted is not installed..."
+    echo -e "\033[0;33m[WARNING]\033[0m gparted is not installed..."
 fi
 
 # cisco packet tracer
@@ -83,7 +87,7 @@ if pkg_installed packettracer
     sudo sed -i "Exec=/opt/packettracer/packettracer %f/c\Exec=env QT_QPA_PLATFORM=xcb /opt/packettracer/packettracer" /usr/share/applications/cisco-pt.desktop
     
 else
-    echo "WARNING: packettracer is not installed..."
+    echo -e "\033[0;33m[WARNING]\033[0m packettracer is not installed..."
 fi
 
 # virtualbox
@@ -98,7 +102,7 @@ if pkg_installed virtualbox
         echo "User is already in vboxusers group, skipping..."
     fi
 else
-  echo "WARNING: virtualbox is not installed..."
+  echo -e "\033[0;33m[WARNING]\033[0m virtualbox is not installed..."
 fi
 
 # shell
