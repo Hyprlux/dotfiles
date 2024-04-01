354667919d6941de6e738cf8e85296b0e0d7b771
Ew4n1011 <ewanlee2005@gmail.com>
2024-04-01 18:38:36 +0200

    Gamemode.sh temporary fix for Hyprland V0.37.1.1

--- Configs/.config/hyprdots/scripts/gamemode.sh
+++ Configs/.config/hyprdots/scripts/gamemode.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env sh
-HYPRGAMEMODE=$(hyprctl getoption animations:enabled | sed -n '2p' | awk '{print $2}')
+HYPRGAMEMODE=$(hyprctl getoption animations:enabled | sed -n '1p' | awk '{print $2}')
 
 # Waybar performance
 FILE="$HOME/.config/waybar/style.css"
 
 sed -i 's/\/\* \(.*animation:.*\) \*\//\1/g' $FILE
+sed -i 's/\/\* \(.*transition:.*\) \*\//\1/g' $FILE
 if [ $HYPRGAMEMODE = 1 ]; then
     sed -i 's/^\(.*animation:.*\)$/\/\* \1 \*\//g' $FILE
 fi
 killall waybar
-waybar > /dev/null 2>&1 &
+waybar >/dev/null 2>&1 &
 
 # Hyprland performance
 if [ $HYPRGAMEMODE = 1 ] ; then
-    hyprctl --batch "\
+        hyprctl --batch "\
         keyword animations:enabled 0;\
         keyword decoration:drop_shadow 0;\
         keyword decoration:blur:enabled 0;\
@@ -22,5 +23,6 @@ if [ $HYPRGAMEMODE = 1 ] ; then
         keyword general:border_size 1;\
         keyword decoration:rounding 0"
     exit
+else
+    hyprctl reload
 fi
-hyprctl reload
